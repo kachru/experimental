58a51b2bcd0b887a80263a44599104ee8220e32d
Sandeep Kachru <skachru@google.com>
2014-02-19 15:57:18 -0800

    updating py files

--- my_first_django_project/my_first_django_project/settings.py
+++ my_first_django_project/my_first_django_project/settings.py
@@ -36,6 +36,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+    'django.contrib.flatpages',    
 )
 
 MIDDLEWARE_CLASSES = (
@@ -80,3 +82,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+
+SITE_ID = 1
+
+TEMPLATE_DIRS = (
+    os.path.join(BASE_DIR, 'templates/'),
+)
